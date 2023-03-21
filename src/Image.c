31ea477ccc2f8aa5b31aee01e39f6d62e5541f39
ehakobya <ehakobya@asu.edu>
2023-03-20 20:42:00 -0700

    Add color shift feature

--- src/Image.c
+++ src/Image.c
@@ -7,11 +7,17 @@
 * @Date 03/20/2023
 * @version 1.0
 */
+
+// INCLUDES
 #include <stdio.h>
 #include <stdlib.h>
 #include "../headers/Image.h"
 #include "../headers/BMPHandler.h"
 
+// FORWARD DECLARATION
+int clamp(int value);
+
+// FUNCTIONS
 Image* image_create(struct Pixel** pArr, int width, int height) {
     Image * new_img = (Image*) malloc(sizeof (pArr));
     new_img->width = width;
@@ -48,9 +54,28 @@ void image_apply_bw(Image* img) {
 }
 
 void image_apply_colorshift(Image* img, int rShift, int gShift, int bShift) {
-    //TODO
+    for (int i = 0; i < img->width; i++) {
+        for (int j = 0; j < img->height; j++) {
+            int newRed = img->pArr[i][j].red + rShift;
+            int newGreen = img->pArr[i][j].green + gShift;
+            int newBlue = img->pArr[i][j].blue + bShift;
+            img->pArr[i][j].red = (unsigned char)((newRed > 255) ? 255 : (newRed < 0) ? 0 : newRed);
+            img->pArr[i][j].green = (unsigned char)((newGreen > 255) ? 255 : (newGreen < 0) ? 0 : newGreen);
+            img->pArr[i][j].blue = (unsigned char)((newBlue > 255) ? 255 : (newBlue < 0) ? 0 : newBlue);
+        }
+    }
 }
 
 void image_apply_resize(Image* img, float factor) {
     //TODO
+}
+
+int clamp(int value) {
+    if (value < 0) {
+        return 0;
+    } else if (value > 255) {
+         return 255;
+    } else {
+        return value;
+    }
 }
